6c8820dec064423f68fd0f38a56548393f9b2ab9
kirimin <cc@kirimin.me>
2014-12-23 12:56:14 +0900

    非同期処理のテストを修正

--- app/src/androidTest/java/me/kirimin/mitsumine/test/BookmarkFeedAccessorTest.java
+++ app/src/androidTest/java/me/kirimin/mitsumine/test/BookmarkFeedAccessorTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,8 +17,6 @@ import me.kirimin.mitsumine.network.BookmarkFeedAccessor.FeedListener;
 import me.kirimin.mitsumine.network.BookmarkFeedAccessor.TYPE;
 import me.kirimin.mitsumine.network.RequestQueueSingleton;
 
-import static org.junit.Assert.assertTrue;
-
 @RunWith(AndroidJUnit4.class)
 public class BookmarkFeedAccessorTest {
 
@@ -24,56 +24,81 @@ public class BookmarkFeedAccessorTest {
     }
 
     @Test
-    public void requestCategoryはFeedを取得できる() {
+    public void requestCategoryはFeedを取得できる() throws InterruptedException {
+        final boolean[] flag = {false};
         BookmarkFeedAccessor.requestCategory(RequestQueueSingleton.getRequestQueue(InstrumentationRegistry.getContext()), new FeedListener() {
 
             @Override
             public void onSuccess(List<Feed> feedList) {
-                if (!feedList.isEmpty()) {
-                    assertTrue(true);
-                }
+                flag[0] = true;
             }
 
             @Override
             public void onError(String errorMessage) {
-                assertTrue(false);
+                Assert.fail();
             }
         }, CATEGORY.MAIN, TYPE.HOT);
+
+        if (!waitForSuccess(flag)) {
+            Assert.fail();
+        }
     }
 
     @Test
-    public void requestKeywordはFeedを取得できる() {
+    public void requestKeywordはFeedを取得できる() throws InterruptedException {
+        final boolean[] flag = {false};
         BookmarkFeedAccessor.requestKeyword(RequestQueueSingleton.getRequestQueue(InstrumentationRegistry.getContext()), new FeedListener() {
 
             @Override
             public void onSuccess(List<Feed> feedList) {
                 if (!feedList.isEmpty()) {
-                    assertTrue(true);
+                    flag[0] = true;
+                } else {
+                    Assert.fail();
                 }
             }
 
             @Override
             public void onError(String errorMessage) {
-                assertTrue(false);
+                Assert.fail();
             }
         }, "Java");
+
+        if (!waitForSuccess(flag)) {
+            Assert.fail();
+        }
     }
 
     @Test
-    public void requestUserBookmarkはFeedを取得できる() {
+    public void requestUserBookmarkはFeedを取得できる() throws InterruptedException {
+        final boolean[] flag = {false};
         BookmarkFeedAccessor.requestUserBookmark(RequestQueueSingleton.getRequestQueue(InstrumentationRegistry.getContext()), new FeedListener() {
 
             @Override
             public void onSuccess(List<Feed> feedList) {
                 if (!feedList.isEmpty()) {
-                    assertTrue(true);
+                    flag[0] = true;
+                } else {
+                    Assert.fail();
                 }
             }
 
             @Override
             public void onError(String errorMessage) {
-                assertTrue(false);
+                Assert.fail();
             }
         }, "hajimepg");
+
+        if (!waitForSuccess(flag)) {
+            Assert.fail();
+        }
+    }
+
+    private boolean waitForSuccess(boolean[] flag) throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            if (flag[0]) return true;
+            Thread.sleep(50);
+        }
+        return false;
     }
 }
