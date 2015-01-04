2c607ae57306e666abfb216dfd08444837be75ad
kirimin <cc@kirimin.me>
2015-01-04 20:36:21 +0900

    ブックマーク登録・削除処理実装

--- app/src/main/java/me/kirimin/mitsumine/network/api/BookmarkApiAccessor.java
+++ app/src/main/java/me/kirimin/mitsumine/network/api/BookmarkApiAccessor.java
@@ -1,5 +1,7 @@
 package me.kirimin.mitsumine.network.api;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
@@ -8,6 +10,7 @@ import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 
 import me.kirimin.mitsumine.model.Account;
+import me.kirimin.mitsumine.model.Bookmark;
 import me.kirimin.mitsumine.network.ApiRequestException;
 import me.kirimin.mitsumine.network.api.oauth.Consumer;
 import me.kirimin.mitsumine.network.api.oauth.HatenaOAuthApi;
@@ -16,7 +19,37 @@ import rx.Subscriber;
 
 public class BookmarkApiAccessor {
 
-    public static Observable<Boolean> requestAddBookmark(final String url, final Account account) {
+    public static Observable<JSONObject> requestAddBookmark(final String url, final Account account) {
+        return Observable.create(new Observable.OnSubscribe<JSONObject>() {
+            @Override
+            public void call(Subscriber<? super JSONObject> subscriber) {
+                OAuthService oAuthService = new ServiceBuilder()
+                        .provider(HatenaOAuthApi.class)
+                        .apiKey(Consumer.K)
+                        .apiSecret(Consumer.S)
+                        .build();
+
+                Token accessToken = new Token(account.token, account.tokenSecret);
+
+                OAuthRequest request = new OAuthRequest(Verb.POST, "http://api.b.hatena.ne.jp/1/my/bookmark");
+                request.addQuerystringParameter("url", url);
+                oAuthService.signRequest(accessToken, request);
+                Response response = request.send();
+                if (response.getCode() != 200) {
+                    subscriber.onError(new ApiRequestException("error code:" + response.getCode()));
+                    return;
+                }
+                try {
+                    subscriber.onNext(new JSONObject(response.getBody()));
+                    subscriber.onCompleted();
+                } catch (JSONException e) {
+                    subscriber.onError(new ApiRequestException(e.getMessage()));
+                }
+            }
+        });
+    }
+
+    public static Observable<Boolean> requestDeleteBookmark(final String url, final Account account) {
         return Observable.create(new Observable.OnSubscribe<Boolean>() {
             @Override
             public void call(Subscriber<? super Boolean> subscriber) {
@@ -28,22 +61,54 @@ public class BookmarkApiAccessor {
 
                 Token accessToken = new Token(account.token, account.tokenSecret);
 
-                String xml = String.format("<entry xmlns='http://purl.org/atom/ns#'>"
-                        + "<title>dummy</title>"
-                        + "<link rel='related' type='text/html' href='%s' />"
-                        + "<summary type='text/plain'></summary>"
-                        + "</entry>", url);
-
-                OAuthRequest request = new OAuthRequest(Verb.POST, "http://b.hatena.ne.jp/atom/post");
-                request.addHeader("Content-Type", "application/octed-stream");
+                OAuthRequest request = new OAuthRequest(Verb.DELETE, "http://api.b.hatena.ne.jp/1/my/bookmark");
+                request.addQuerystringParameter("url", url);
                 oAuthService.signRequest(accessToken, request);
-                request.addPayload(xml);
                 Response response = request.send();
-                if (response.getCode() == 201) {
-                    subscriber.onNext(true);
-                    subscriber.onCompleted();
-                } else {
+                if (response.getCode() != 204) {
                     subscriber.onError(new ApiRequestException("error code:" + response.getCode()));
+                    return;
+                }
+                subscriber.onNext(true);
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    public static Observable<Boolean> requestEditBookmark() {
+        return Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(Subscriber<? super Boolean> subscriber) {
+
+            }
+        });
+    }
+
+    public static Observable<JSONObject> requestIsAlreadyBookmark(final String url, final Account account) {
+        return Observable.create(new Observable.OnSubscribe<JSONObject>() {
+            @Override
+            public void call(Subscriber<? super JSONObject> subscriber) {
+                OAuthService oAuthService = new ServiceBuilder()
+                        .provider(HatenaOAuthApi.class)
+                        .apiKey(Consumer.K)
+                        .apiSecret(Consumer.S)
+                        .build();
+
+                Token accessToken = new Token(account.token, account.tokenSecret);
+
+                OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.b.hatena.ne.jp/1/my/bookmark");
+                request.addQuerystringParameter("url", url);
+                oAuthService.signRequest(accessToken, request);
+                Response response = request.send();
+                if (response.getCode() != 200) {
+                    subscriber.onError(new ApiRequestException("error code:" + response.getCode()));
+                    return;
+                }
+                try {
+                    subscriber.onNext(new JSONObject(response.getBody()));
+                    subscriber.onCompleted();
+                } catch (JSONException e) {
+                    subscriber.onError(new ApiRequestException(e.getMessage()));
                 }
             }
         });
--- app/src/main/java/me/kirimin/mitsumine/ui/fragment/RegisterBookmarkFragment.java
+++ app/src/main/java/me/kirimin/mitsumine/ui/fragment/RegisterBookmarkFragment.java
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONObject;
 
 import me.kirimin.mitsumine.R;
 import me.kirimin.mitsumine.db.AccountDAO;
@@ -15,6 +18,8 @@ import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class RegisterBookmarkFragment extends Fragment {
+
+    private boolean isAlreadyBookmark;
 
     public static RegisterBookmarkFragment newFragment(String url) {
         RegisterBookmarkFragment fragment = new RegisterBookmarkFragment();
@@ -31,26 +36,74 @@ public class RegisterBookmarkFragment extends Fragment {
             throw new IllegalStateException("url is null");
         }
         final View rootView = inflater.inflate(R.layout.fragment_register_bookmark, container, false);
+
+        BookmarkApiAccessor.requestIsAlreadyBookmark(url, AccountDAO.get())
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<JSONObject>() {
+                    @Override
+                    public void call(JSONObject object) {
+                        changeBookmarkStatus(true);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        changeBookmarkStatus(false);
+                    }
+                });
         rootView.findViewById(R.id.RegisterBookmarkRegisterButton).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
+                v.setEnabled(false);
                 TextView comment = (TextView) rootView.findViewById(R.id.RegisterBookmarkCommentEditText);
-                BookmarkApiAccessor.requestAddBookmark(url, AccountDAO.get())
-                        .subscribeOn(Schedulers.newThread())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(new Action1<Boolean>() {
-                            @Override
-                            public void call(Boolean aBoolean) {
+                if (isAlreadyBookmark) {
+                    BookmarkApiAccessor.requestDeleteBookmark(url, AccountDAO.get())
+                            .subscribeOn(Schedulers.newThread())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(new Action1<Boolean>() {
+                                @Override
+                                public void call(Boolean aBoolean) {
+                                    changeBookmarkStatus(false);
+                                    v.setEnabled(true);
+                                }
+                            }, new Action1<Throwable>() {
+                                @Override
+                                public void call(Throwable throwable) {
+                                    v.setEnabled(true);
+                                }
+                            });
 
-                            }
-                        }, new Action1<Throwable>() {
-                            @Override
-                            public void call(Throwable throwable) {
-
-                            }
-                        });
+                } else {
+                    BookmarkApiAccessor.requestAddBookmark(url, AccountDAO.get())
+                            .subscribeOn(Schedulers.newThread())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(new Action1<JSONObject>() {
+                                @Override
+                                public void call(JSONObject object) {
+                                    changeBookmarkStatus(true);
+                                    v.setEnabled(true);
+                                }
+                            }, new Action1<Throwable>() {
+                                @Override
+                                public void call(Throwable throwable) {
+                                    v.setEnabled(true);
+                                }
+                            });
+                }
             }
         });
         return rootView;
+    }
+
+    private void changeBookmarkStatus(boolean isAlreadyBookmark) {
+        if (getView() == null) return;
+
+        this.isAlreadyBookmark = isAlreadyBookmark;
+        Button button = (Button) getView().findViewById(R.id.RegisterBookmarkRegisterButton);
+        if (isAlreadyBookmark) {
+            button.setText("削除");
+        } else {
+            button.setText("登録");
+        }
     }
 }
