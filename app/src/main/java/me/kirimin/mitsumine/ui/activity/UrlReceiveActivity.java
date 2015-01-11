3f52bb177a04da292acd9c97a333d61c65957b42
kirimin <cc@kirimin.me>
2015-01-11 16:57:27 +0900

    UrlReceiveActivityからの遷移先をネイティブ画面に

--- app/src/main/java/me/kirimin/mitsumine/ui/activity/UrlReceiveActivity.java
+++ app/src/main/java/me/kirimin/mitsumine/ui/activity/UrlReceiveActivity.java
@@ -1,9 +1,13 @@
 package me.kirimin.mitsumine.ui.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+
+import me.kirimin.mitsumine.R;
 
 public class UrlReceiveActivity extends ActionBarActivity {
 
@@ -12,7 +16,14 @@ public class UrlReceiveActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         if (Intent.ACTION_SEND.equals(getIntent().getAction())) {
             String url = getIntent().getExtras().getCharSequence(Intent.EXTRA_TEXT).toString();
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://b.hatena.ne.jp/entry/" + url)));
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            if (pref.getBoolean(getString(R.string.key_use_browser_to_comment_list), false)) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://b.hatena.ne.jp/entry/" + url)));
+            } else {
+                Intent intent = new Intent(getApplicationContext(), EntryInfoActivity.class);
+                intent.putExtras(EntryInfoActivity.buildBundle(url));
+                startActivity(intent);
+            }
         }
         finish();
     }
