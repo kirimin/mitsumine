284870da982345b50d40ff0c3552795f43c5f44f
kirimin <cc@kirimin.me>
2015-01-21 21:38:14 +0900

    BookmarkCountApiを実装

--- app/src/main/java/me/kirimin/mitsumine/network/api/ApiAccessor.java
+++ app/src/main/java/me/kirimin/mitsumine/network/api/ApiAccessor.java
@@ -3,6 +3,7 @@ package me.kirimin.mitsumine.network.api;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONObject;
 
@@ -22,6 +23,23 @@ class ApiAccessor {
                 requestQueue.add(new JsonObjectRequest(url, null, future, future));
                 try {
                     JSONObject response = future.get();
+                    subscriber.onNext(response);
+                    subscriber.onCompleted();
+                } catch (InterruptedException | ExecutionException e) {
+                    subscriber.onError(new ApiRequestException(e.getMessage()));
+                }
+            }
+        });
+    }
+
+    static Observable<String> stringRequest(final RequestQueue requestQueue, final String url) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(final Subscriber<? super String> subscriber) {
+                RequestFuture<String> future = RequestFuture.newFuture();
+                requestQueue.add(new StringRequest(url, future, future));
+                try {
+                    String response = future.get();
                     subscriber.onNext(response);
                     subscriber.onCompleted();
                 } catch (InterruptedException | ExecutionException e) {
