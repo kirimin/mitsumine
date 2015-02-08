26a1486feb8b9597bc8a60db5fb2a7898e634b07
kirimin <cc@kirimin.me>
2015-02-08 21:53:17 +0900

    更読み実装

--- app/src/main/java/me/kirimin/mitsumine/ui/fragment/MyBookmarksFragment.java
+++ app/src/main/java/me/kirimin/mitsumine/ui/fragment/MyBookmarksFragment.java
@@ -3,16 +3,21 @@ package me.kirimin.mitsumine.ui.fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import me.kirimin.mitsumine.R;
 import me.kirimin.mitsumine.db.AccountDAO;
 import me.kirimin.mitsumine.model.Account;
@@ -23,11 +28,11 @@ import me.kirimin.mitsumine.ui.adapter.MyBookmarksAdapter;
 import me.kirimin.mitsumine.util.MyBookmarksFunc;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
-public class MyBookmarksFragment extends Fragment implements MyBookmarksAdapter.OnMyBookmarkClickListener, SwipeRefreshLayout.OnRefreshListener {
+public class MyBookmarksFragment extends Fragment implements MyBookmarksAdapter.OnMyBookmarkClickListener, SwipeRefreshLayout.OnRefreshListener, AbsListView.OnScrollListener {
 
     public static MyBookmarksFragment newFragment(String keyword) {
         MyBookmarksFragment fragment = new MyBookmarksFragment();
@@ -37,16 +42,25 @@ public class MyBookmarksFragment extends Fragment implements MyBookmarksAdapter.
         return fragment;
     }
 
+    @InjectView(R.id.MyBookmarksListView)
+    ListView listView;
+    @InjectView(R.id.MyBookmarksSwipeLayout)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     private CompositeSubscription subscriptions = new CompositeSubscription();
-    private SwipeRefreshLayout swipeRefreshLayout;
+    private int total = 0;
+    private MyBookmarksAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_my_bookmarks, container, false);
-        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.MyBookmarksSwipeLayout);
+        ButterKnife.inject(this, rootView);
         swipeRefreshLayout.setColorSchemeResources(R.color.blue, R.color.orange);
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setProgressViewOffset(false, 0, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources().getDisplayMetrics()));
+        adapter = new MyBookmarksAdapter(getActivity(), this);
+        listView.setAdapter(adapter);
+        listView.setOnScrollListener(MyBookmarksFragment.this);
         return rootView;
     }
 
@@ -76,9 +90,20 @@ public class MyBookmarksFragment extends Fragment implements MyBookmarksAdapter.
 
     @Override
     public void onRefresh() {
+        adapter.clear();
         requestApi();
     }
 
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (firstVisibleItem + visibleItemCount == totalItemCount && adapter.getCount() < total && !swipeRefreshLayout.isRefreshing()) {
+            requestApi(adapter.getCount());
+        }
+    }
 
     private void requestApi() {
         requestApi(0);
@@ -90,17 +115,24 @@ public class MyBookmarksFragment extends Fragment implements MyBookmarksAdapter.
             return;
         }
         Account account = AccountDAO.get();
-        final ListView listView = (ListView) getView().findViewById(R.id.MyBookmarksListView);
-        final MyBookmarksAdapter adapter = new MyBookmarksAdapter(getActivity(), this);
         swipeRefreshLayout.setRefreshing(true);
         subscriptions.add(MyBookmarksApi.request(account, getArguments().getString("keyword"), offset)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<JSONObject, JSONObject>() {
+                    @Override
+                    public JSONObject call(JSONObject jsonObject) {
+                        try {
+                            total = jsonObject.getJSONObject("meta").getInt("total");
+                        } catch (JSONException e) {
+                        }
+                        return jsonObject;
+                    }
+                })
                 .flatMap(MyBookmarksFunc.mapToMyBookmarkList())
                 .subscribe(new Observer<MyBookmark>() {
                     @Override
                     public void onCompleted() {
-                        listView.setAdapter(adapter);
                         swipeRefreshLayout.setRefreshing(false);
                     }
 
