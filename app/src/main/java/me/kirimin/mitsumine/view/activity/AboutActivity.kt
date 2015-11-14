d11eb3e1ca540ec1b3675581eb99934b0152d4c8
kirimin <cc@kirimin.me>
2015-11-14 20:13:23 +0900

    Removed a unused import.

--- app/src/main/java/me/kirimin/mitsumine/view/activity/AboutActivity.kt
+++ app/src/main/java/me/kirimin/mitsumine/view/activity/AboutActivity.kt
@@ -3,7 +3,6 @@ package me.kirimin.mitsumine.view.activity
 import me.kirimin.mitsumine.R
 
 import android.os.Bundle
-import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
